1537203b3bdf3a199cfd8140f178ff3118fb04ce
Honghao Zhang <zhh358@gmail.com>
2016-06-24 20:38:00 -0400

    Add `getColors()` method in main thread. Add documentations.

--- UIImageColors.swift
+++ UIImageColors.swift
@@ -93,154 +93,16 @@ extension UIImage {
         return result
     }
     
-    public func getColors(completionHandler: (UIImageColors) -> Void) {
-        let ratio = self.size.width/self.size.height
-        let r_width: CGFloat = 250
-        
-        self.getColors(CGSizeMake(r_width, r_width/ratio), completionHandler: completionHandler)
-    }
-    
-    public func getColors(scaleDownSize: CGSize, completionHandler: (UIImageColors) -> Void) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            var result = UIImageColors()
-            
-            let cgImage = self.resize(scaleDownSize).CGImage
-            let width = CGImageGetWidth(cgImage)
-            let height = CGImageGetHeight(cgImage)
-            
-            let bytesPerPixel: Int = 4
-            let bytesPerRow: Int = width * bytesPerPixel
-            let bitsPerComponent: Int = 8
-            let randomColorsThreshold = Int(CGFloat(height)*0.01)
-            let sortedColorComparator: NSComparator = { (main, other) -> NSComparisonResult in
-                let m = main as! PCCountedColor, o = other as! PCCountedColor
-                if m.count < o.count {
-                    return NSComparisonResult.OrderedDescending
-                } else if m.count == o.count {
-                    return NSComparisonResult.OrderedSame
-                } else {
-                    return NSComparisonResult.OrderedAscending
-                }
-            }
-            let blackColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-            let whiteColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-            
-            let colorSpace = CGColorSpaceCreateDeviceRGB()
-            let raw = malloc(bytesPerRow * height)
-            let bitmapInfo = CGImageAlphaInfo.PremultipliedFirst.rawValue
-            let ctx = CGBitmapContextCreate(raw, width, height, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo)
-            CGContextDrawImage(ctx, CGRectMake(0, 0, CGFloat(width), CGFloat(height)), cgImage)
-            let data = UnsafePointer<UInt8>(CGBitmapContextGetData(ctx))
-            
-            let leftEdgeColors = NSCountedSet(capacity: height)
-            let imageColors = NSCountedSet(capacity: width * height)
-            
-            for x in 0..<width {
-                for y in 0..<height {
-                    let pixel = ((width * y) + x) * bytesPerPixel
-                    let color = UIColor(
-                        red: CGFloat(data[pixel+1])/255,
-                        green: CGFloat(data[pixel+2])/255,
-                        blue: CGFloat(data[pixel+3])/255,
-                        alpha: 1
-                    )
-                    
-                    // A lot of albums have white or black edges from crops, so ignore the first few pixels
-                    if 5 <= x && x <= 10 {
-                        leftEdgeColors.addObject(color)
-                    }
-                    
-                    imageColors.addObject(color)
-                }
-            }
-            
-            // Get background color
-            var enumerator = leftEdgeColors.objectEnumerator()
-            var sortedColors = NSMutableArray(capacity: leftEdgeColors.count)
-            while let kolor = enumerator.nextObject() as? UIColor {
-                let colorCount = leftEdgeColors.countForObject(kolor)
-                if randomColorsThreshold < colorCount  {
-                    sortedColors.addObject(PCCountedColor(color: kolor, count: colorCount))
-                }
-            }
-            sortedColors.sortUsingComparator(sortedColorComparator)
-            
-            var proposedEdgeColor: PCCountedColor
-            if 0 < sortedColors.count {
-                proposedEdgeColor = sortedColors.objectAtIndex(0) as! PCCountedColor
-            } else {
-                proposedEdgeColor = PCCountedColor(color: blackColor, count: 1)
-            }
-            
-            if proposedEdgeColor.color.isBlackOrWhite && 0 < sortedColors.count {
-                for i in 1..<sortedColors.count {
-                    let nextProposedEdgeColor = sortedColors.objectAtIndex(i) as! PCCountedColor
-                    if (CGFloat(nextProposedEdgeColor.count)/CGFloat(proposedEdgeColor.count)) > 0.3 {
-                        if !nextProposedEdgeColor.color.isBlackOrWhite {
-                            proposedEdgeColor = nextProposedEdgeColor
-                            break
-                        }
-                    } else {
-                        break
-                    }
-                }
-            }
-            result.backgroundColor = proposedEdgeColor.color
-            
-            // Get foreground colors
-            enumerator = imageColors.objectEnumerator()
-            sortedColors.removeAllObjects()
-            sortedColors = NSMutableArray(capacity: imageColors.count)
-            let findDarkTextColor = !result.backgroundColor.isDarkColor
-            
-            while var kolor = enumerator.nextObject() as? UIColor {
-                kolor = kolor.colorWithMinimumSaturation(0.15)
-                if kolor.isDarkColor == findDarkTextColor {
-                    let colorCount = imageColors.countForObject(kolor)
-                    sortedColors.addObject(PCCountedColor(color: kolor, count: colorCount))
-                }
-            }
-            sortedColors.sortUsingComparator(sortedColorComparator)
-            
-            for curContainer in sortedColors {
-                let kolor = (curContainer as! PCCountedColor).color
-                
-                if result.primaryColor == nil {
-                    if kolor.isContrastingColor(result.backgroundColor) {
-                        result.primaryColor = kolor
-                    }
-                } else if result.secondaryColor == nil {
-                    if !result.primaryColor.isDistinct(kolor) || !kolor.isContrastingColor(result.backgroundColor) {
-                        continue
-                    }
-                    
-                    result.secondaryColor = kolor
-                } else if result.detailColor == nil {
-                    if !result.secondaryColor.isDistinct(kolor) || !result.primaryColor.isDistinct(kolor) || !kolor.isContrastingColor(result.backgroundColor) {
-                        continue
-                    }
-                    
-                    result.detailColor = kolor
-                    break
-                }
-            }
-            
-            let isDarkBackgound = result.backgroundColor.isDarkColor
-            
-            if result.primaryColor == nil {
-                result.primaryColor = isDarkBackgound ? whiteColor:blackColor
-            }
-            
-            if result.secondaryColor == nil {
-                result.secondaryColor = isDarkBackgound ? whiteColor:blackColor
-            }
-            
-            if result.detailColor == nil {
-                result.detailColor = isDarkBackgound ? whiteColor:blackColor
-            }
-            
-            // Release the allocated memory
-            free(raw)
+    /**
+     Get `UIImageColors` from the image asynchronously (in background thread).
+     Discussion: Use smaller sizes for better performance at the cost of quality colors. Use larger sizes for better color sampling and quality at the cost of performance.
+     
+     - parameter scaleDownSize:     Downscale size of image for sampling, if `CGSize.zero` is provided, the sample image is rescaled to a width of 250px and the aspect ratio height.
+     - parameter completionHandler: `UIImageColors` for this image.
+     */
+    public func getColors(scaleDownSize: CGSize = CGSize.zero, completionHandler: (UIImageColors) -> Void) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { [unowned self] in
+            let result = self.getColors(scaleDownSize)
             
             dispatch_async(dispatch_get_main_queue()) {
                 completionHandler(result)
@@ -248,4 +110,162 @@ extension UIImage {
         }
     }
     
+    /**
+     Get `UIImageColors` from the image synchronously (in main thread).
+     Discussion: Use smaller sizes for better performance at the cost of quality colors. Use larger sizes for better color sampling and quality at the cost of performance.
+     
+     - parameter scaleDownSize: Downscale size of image for sampling, if `CGSize.zero` is provided, the sample image is rescaled to a width of 250px and the aspect ratio height.
+     
+     - returns: `UIImageColors` for this image.
+     */
+    public func getColors(scaleDownSize: CGSize = CGSize.zero) -> UIImageColors {
+        var scaleDownSize = scaleDownSize
+        if scaleDownSize == CGSize.zero {
+            let ratio = self.size.width/self.size.height
+            let r_width: CGFloat = 250
+            scaleDownSize = CGSizeMake(r_width, r_width/ratio)
+        }
+        
+        var result = UIImageColors()
+        
+        let cgImage = self.resize(scaleDownSize).CGImage
+        let width = CGImageGetWidth(cgImage)
+        let height = CGImageGetHeight(cgImage)
+        
+        let bytesPerPixel: Int = 4
+        let bytesPerRow: Int = width * bytesPerPixel
+        let bitsPerComponent: Int = 8
+        let randomColorsThreshold = Int(CGFloat(height)*0.01)
+        let sortedColorComparator: NSComparator = { (main, other) -> NSComparisonResult in
+            let m = main as! PCCountedColor, o = other as! PCCountedColor
+            if m.count < o.count {
+                return NSComparisonResult.OrderedDescending
+            } else if m.count == o.count {
+                return NSComparisonResult.OrderedSame
+            } else {
+                return NSComparisonResult.OrderedAscending
+            }
+        }
+        let blackColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        let whiteColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let raw = malloc(bytesPerRow * height)
+        let bitmapInfo = CGImageAlphaInfo.PremultipliedFirst.rawValue
+        let ctx = CGBitmapContextCreate(raw, width, height, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo)
+        CGContextDrawImage(ctx, CGRectMake(0, 0, CGFloat(width), CGFloat(height)), cgImage)
+        let data = UnsafePointer<UInt8>(CGBitmapContextGetData(ctx))
+        
+        let leftEdgeColors = NSCountedSet(capacity: height)
+        let imageColors = NSCountedSet(capacity: width * height)
+        
+        for x in 0..<width {
+            for y in 0..<height {
+                let pixel = ((width * y) + x) * bytesPerPixel
+                let color = UIColor(
+                    red: CGFloat(data[pixel+1])/255,
+                    green: CGFloat(data[pixel+2])/255,
+                    blue: CGFloat(data[pixel+3])/255,
+                    alpha: 1
+                )
+                
+                // A lot of albums have white or black edges from crops, so ignore the first few pixels
+                if 5 <= x && x <= 10 {
+                    leftEdgeColors.addObject(color)
+                }
+                
+                imageColors.addObject(color)
+            }
+        }
+        
+        // Get background color
+        var enumerator = leftEdgeColors.objectEnumerator()
+        var sortedColors = NSMutableArray(capacity: leftEdgeColors.count)
+        while let kolor = enumerator.nextObject() as? UIColor {
+            let colorCount = leftEdgeColors.countForObject(kolor)
+            if randomColorsThreshold < colorCount  {
+                sortedColors.addObject(PCCountedColor(color: kolor, count: colorCount))
+            }
+        }
+        sortedColors.sortUsingComparator(sortedColorComparator)
+        
+        var proposedEdgeColor: PCCountedColor
+        if 0 < sortedColors.count {
+            proposedEdgeColor = sortedColors.objectAtIndex(0) as! PCCountedColor
+        } else {
+            proposedEdgeColor = PCCountedColor(color: blackColor, count: 1)
+        }
+        
+        if proposedEdgeColor.color.isBlackOrWhite && 0 < sortedColors.count {
+            for i in 1..<sortedColors.count {
+                let nextProposedEdgeColor = sortedColors.objectAtIndex(i) as! PCCountedColor
+                if (CGFloat(nextProposedEdgeColor.count)/CGFloat(proposedEdgeColor.count)) > 0.3 {
+                    if !nextProposedEdgeColor.color.isBlackOrWhite {
+                        proposedEdgeColor = nextProposedEdgeColor
+                        break
+                    }
+                } else {
+                    break
+                }
+            }
+        }
+        result.backgroundColor = proposedEdgeColor.color
+        
+        // Get foreground colors
+        enumerator = imageColors.objectEnumerator()
+        sortedColors.removeAllObjects()
+        sortedColors = NSMutableArray(capacity: imageColors.count)
+        let findDarkTextColor = !result.backgroundColor.isDarkColor
+        
+        while var kolor = enumerator.nextObject() as? UIColor {
+            kolor = kolor.colorWithMinimumSaturation(0.15)
+            if kolor.isDarkColor == findDarkTextColor {
+                let colorCount = imageColors.countForObject(kolor)
+                sortedColors.addObject(PCCountedColor(color: kolor, count: colorCount))
+            }
+        }
+        sortedColors.sortUsingComparator(sortedColorComparator)
+        
+        for curContainer in sortedColors {
+            let kolor = (curContainer as! PCCountedColor).color
+            
+            if result.primaryColor == nil {
+                if kolor.isContrastingColor(result.backgroundColor) {
+                    result.primaryColor = kolor
+                }
+            } else if result.secondaryColor == nil {
+                if !result.primaryColor.isDistinct(kolor) || !kolor.isContrastingColor(result.backgroundColor) {
+                    continue
+                }
+                
+                result.secondaryColor = kolor
+            } else if result.detailColor == nil {
+                if !result.secondaryColor.isDistinct(kolor) || !result.primaryColor.isDistinct(kolor) || !kolor.isContrastingColor(result.backgroundColor) {
+                    continue
+                }
+                
+                result.detailColor = kolor
+                break
+            }
+        }
+        
+        let isDarkBackgound = result.backgroundColor.isDarkColor
+        
+        if result.primaryColor == nil {
+            result.primaryColor = isDarkBackgound ? whiteColor:blackColor
+        }
+        
+        if result.secondaryColor == nil {
+            result.secondaryColor = isDarkBackgound ? whiteColor:blackColor
+        }
+        
+        if result.detailColor == nil {
+            result.detailColor = isDarkBackgound ? whiteColor:blackColor
+        }
+        
+        // Release the allocated memory
+        free(raw)
+        
+        return result
+    }
 }
