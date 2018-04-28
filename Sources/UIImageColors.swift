f1c58785ffa43d2e4aa86e9f62a1561e37f99012
serafino sorleti <mr.sorleti+dev@gmail.com>
2018-04-29 00:29:21 +0200

    getColors now visible to Objective-C code

--- Sources/UIImageColors.swift
+++ Sources/UIImageColors.swift
@@ -313,5 +313,134 @@ extension UIImage {
             detail: proposed[3].uicolor
         )
     }
+    
+    @objc(colorsOfImage)
+    public func getColors() -> Array<UIColor> {
+        var scaleDownSize: CGSize = self.size
+        let quality = UIImageColorsQuality.high
+        if quality != .highest {
+            if self.size.width < self.size.height {
+                let ratio = self.size.height/self.size.width
+                scaleDownSize = CGSize(width: quality.rawValue/ratio, height: quality.rawValue)
+            } else {
+                let ratio = self.size.width/self.size.height
+                scaleDownSize = CGSize(width: quality.rawValue, height: quality.rawValue/ratio)
+            }
+        }
+        
+        let cgImage = self.resizeForUIImageColors(newSize: scaleDownSize).cgImage!
+        let width: Int = cgImage.width
+        let height: Int = cgImage.height
+        
+        let threshold = Int(CGFloat(height)*0.01)
+        var proposed: [Double] = [-1,-1,-1,-1]
+        
+        guard let data = CFDataGetBytePtr(cgImage.dataProvider!.data) else {
+            fatalError("UIImageColors.getColors failed: could not get cgImage data.")
+        }
+        
+        let imageColors = NSCountedSet(capacity: width*height)
+        for x in 0..<width {
+            for y in 0..<height {
+                let pixel: Int = ((width * y) + x) * 4
+                if 127 <= data[pixel+3] {
+                    imageColors.add((Double(data[pixel+2])*1000000)+(Double(data[pixel+1])*1000)+(Double(data[pixel])))
+                }
+            }
+        }
+        
+        let sortedColorComparator: Comparator = { (main, other) -> ComparisonResult in
+            let m = main as! UIImageColorsCounter, o = other as! UIImageColorsCounter
+            if m.count < o.count {
+                return .orderedDescending
+            } else if m.count == o.count {
+                return .orderedSame
+            } else {
+                return .orderedAscending
+            }
+        }
+        
+        var enumerator = imageColors.objectEnumerator()
+        var sortedColors = NSMutableArray(capacity: imageColors.count)
+        while let K = enumerator.nextObject() as? Double {
+            let C = imageColors.count(for: K)
+            if threshold < C {
+                sortedColors.add(UIImageColorsCounter(color: K, count: C))
+            }
+        }
+        sortedColors.sort(comparator: sortedColorComparator)
+        
+        var proposedEdgeColor: UIImageColorsCounter
+        if 0 < sortedColors.count {
+            proposedEdgeColor = sortedColors.object(at: 0) as! UIImageColorsCounter
+        } else {
+            proposedEdgeColor = UIImageColorsCounter(color: 0, count: 1)
+        }
+        
+        if proposedEdgeColor.color.isBlackOrWhite && 0 < sortedColors.count {
+            for i in 1..<sortedColors.count {
+                let nextProposedEdgeColor = sortedColors.object(at: i) as! UIImageColorsCounter
+                if Double(nextProposedEdgeColor.count)/Double(proposedEdgeColor.count) > 0.3 {
+                    if !nextProposedEdgeColor.color.isBlackOrWhite {
+                        proposedEdgeColor = nextProposedEdgeColor
+                        break
+                    }
+                } else {
+                    break
+                }
+            }
+        }
+        proposed[0] = proposedEdgeColor.color
+        
+        enumerator = imageColors.objectEnumerator()
+        sortedColors.removeAllObjects()
+        sortedColors = NSMutableArray(capacity: imageColors.count)
+        let findDarkTextColor = !proposed[0].isDarkColor
+        
+        while var K = enumerator.nextObject() as? Double {
+            K = K.with(minSaturation: 0.15)
+            if K.isDarkColor == findDarkTextColor {
+                let C = imageColors.count(for: K)
+                sortedColors.add(UIImageColorsCounter(color: K, count: C))
+            }
+        }
+        sortedColors.sort(comparator: sortedColorComparator)
+        
+        for color in sortedColors {
+            let color = (color as! UIImageColorsCounter).color
+            
+            if proposed[1] == -1 {
+                if color.isContrasting(proposed[0]) {
+                    proposed[1] = color
+                }
+            } else if proposed[2] == -1 {
+                if !color.isContrasting(proposed[0]) || !proposed[1].isDistinct(color) {
+                    continue
+                }
+                proposed[2] = color
+            } else if proposed[3] == -1 {
+                if !color.isContrasting(proposed[0]) || !proposed[2].isDistinct(color) || !proposed[1].isDistinct(color) {
+                    continue
+                }
+                proposed[3] = color
+                break
+            }
+        }
+        
+        let isDarkBackground = proposed[0].isDarkColor
+        for i in 1...3 {
+            if proposed[i] == -1 {
+                proposed[i] = isDarkBackground ? 255255255:0
+            }
+        }
+        
+        /*
+         background: proposed[0].uicolor,
+         primary: proposed[1].uicolor,
+         secondary: proposed[1].uicolor,
+         detail: proposed[3].uicolor
+         */
+        return [proposed[0].uicolor, proposed[1].uicolor, proposed[1].uicolor, proposed[3].uicolor]
+    }
 }
 
