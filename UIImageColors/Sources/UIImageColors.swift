90f64cd64f0a20e786bca739ef06ca592dee2b88
Nicholas Cooke <ncooke3@gatech.edu>
2019-07-13 23:19:25 -0700

    Migration to Swift 5 and silenced warnings

--- UIImageColors/Sources/UIImageColors.swift
+++ UIImageColors/Sources/UIImageColors.swift
@@ -62,15 +62,15 @@ fileprivate extension Double {
         return fmod(self,1000)
     }
     
-    fileprivate var isDarkColor: Bool {
+    var isDarkColor: Bool {
         return (r*0.2126) + (g*0.7152) + (b*0.0722) < 127.5
     }
     
-    fileprivate var isBlackOrWhite: Bool {
+    var isBlackOrWhite: Bool {
         return (r > 232 && g > 232 && b > 232) || (r < 23 && g < 23 && b < 23)
     }
     
-    fileprivate func isDistinct(_ other: Double) -> Bool {
+    func isDistinct(_ other: Double) -> Bool {
         let _r = self.r
         let _g = self.g
         let _b = self.b
@@ -82,7 +82,7 @@ fileprivate extension Double {
             && !(fabs(_r-_g) < 7.65 && fabs(_r-_b) < 7.65 && fabs(o_r-o_g) < 7.65 && fabs(o_r-o_b) < 7.65)
     }
     
-    fileprivate func with(minSaturation: Double) -> Double {
+    func with(minSaturation: Double) -> Double {
         // Ref: https://en.wikipedia.org/wiki/HSL_and_HSV
         
         // Convert RGB to HSV
@@ -157,7 +157,7 @@ fileprivate extension Double {
         return (floor((R + m)*255)*1000000)+(floor((G + m)*255)*1000)+floor((B + m)*255)
     }
     
-    fileprivate func isContrasting(_ color: Double) -> Bool {
+    func isContrasting(_ color: Double) -> Bool {
         let bgLum = (0.2126*r)+(0.7152*g)+(0.0722*b)+12.75
         let fgLum = (0.2126*color.r)+(0.7152*color.g)+(0.0722*color.b)+12.75
         if bgLum > fgLum {
@@ -167,11 +167,11 @@ fileprivate extension Double {
         }
     }
     
-    fileprivate var uicolor: UIColor {
+    var uicolor: UIColor {
         return UIColor(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: 1)
     }
     
-    fileprivate var pretty: String {
+    var pretty: String {
         return "\(Int(self.r)), \(Int(self.g)), \(Int(self.b))"
     }
 }
