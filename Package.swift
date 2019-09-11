4dc840332e35cbb2b1a49dae5656d22343d453c1
Brandon Evans <brandon@brandonevans.ca>
2019-09-10 21:49:35 -0600

    Update Package.swift with library product

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-  name: "UIImageColors"
+    name: "UIImageColors",
+    products: [
+        .library(name: "UIImageColors", targets: ["UIImageColors"])
+    ],
+    targets: [
+        .target(name: "UIImageColors", path: "UIImageColors")
+    ]
 )
